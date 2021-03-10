abf076ff14836db883924dab2ab8d0c044ad8bec
Andy Chentsov <chentsov@gmail.com>
2021-03-10 15:59:23 +0300

    Codedoc

--- package/lib/src/generic_field/controller.dart
+++ package/lib/src/generic_field/controller.dart
@@ -2,6 +2,25 @@ library easy_form;
 
 import 'package:flutter/foundation.dart';
 
+/// Base controller class for use with custom form fields.
+///
+/// It can be used as an ancestor for a controller of a
+/// specific type, or directly without creating a descendant:
+/// ```dart
+/// EasyCustomFormField<Color, EasyFormFieldController<Color>>(
+///   name: 'color',
+///   initialValue: Colors.teal,
+///   controllerBuilder: (value) => EasyFormFieldController<Color>(value),
+///   builder: (fieldState, onChangedHandler) => ColorField(
+///     controller: fieldState.controller,
+///     onChange: onChangedHandler,
+///   ),
+/// )
+/// ```
+///
+/// See also:
+///
+///  * [EasyFormGenericField] for a usage example.
 class EasyFormFieldController<T> extends ValueNotifier<T> {
   T _value;
 
--- package/lib/src/generic_field/field.dart
+++ package/lib/src/generic_field/field.dart
@@ -1,6 +1,69 @@
 import 'package:flutter/widgets.dart';
 import 'controller.dart';
 
+/// Base class for creating a form field.
+///
+/// It can be used as a basis for creating a custom field for a form,
+/// where controller support and processing of value changes
+/// are already implemented.
+///
+/// The value property allows you to read the value of the associated
+/// controller and change it; when the value changes, the field widget
+/// will also be rebuilt.
+///
+/// The descendant at least needs to implement a constructor
+/// and the `build` method.
+///
+/// An example of implementing a custom form field for choosing a color,
+/// together with the [EasyFormFieldController] constructor:
+/// ```dart
+/// class ColorController extends EasyFormFieldController<Color> {
+///   ColorController(Color value) : super(value);
+/// }
+///
+/// class ColorField extends EasyFormGenericField<Color> {
+///   ColorField({
+///     Key key,
+///     @required ColorController controller,
+///     ValueChanged<Color> onChange,
+///   }) : super(
+///           key: key,
+///           controller: controller,
+///           onChange: onChange,
+///         );
+///
+///   void _change() {
+///     value = _getRandomColor();
+///   }
+///
+///   Color _getRandomColor() {
+///     return Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
+///   }
+///
+///   @override
+///   Widget build(BuildContext context) {
+///     return Align(
+///       alignment: Alignment.centerLeft,
+///       child: GestureDetector(
+///         onTap: _change,
+///         child: Container(
+///           width: 50,
+///           height: 50,
+///           decoration: BoxDecoration(
+///             color: value,
+///             border: Border.all(
+///               color: Color(0xFF000000),
+///               width: 2,
+///             ),
+///             shape: BoxShape.circle,
+///           ),
+///         ),
+///       ),
+///     );
+///   }
+/// }
+/// ```
+///
 abstract class EasyFormGenericField<T> extends StatefulWidget {
   final EasyFormFieldController<T> controller;
   final ValueChanged<T> onChange;
